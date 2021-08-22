b734ae4ef69a6fe93d345e69cecf255d7a6044d6
Svyatoslav29 <svyatoslav29@gmail.com>
2021-08-22 19:47:10 +0300

    timer

--- index.js
+++ index.js
@@ -9,5 +9,31 @@ const colors = [
   
   const randomIntegerFromInterval = (min, max) => {
       return Math.floor(Math.random() * (max - min + 1) + min);
-    };
+    };  
+
+  const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+console.log(refs);
+refs.startBtn.addEventListener('click', startBtnClick);
+refs.stopBtn.addEventListener('click', stopBtnClick);
+refs.startBtn.disabled = false;
+
+function timer() {
+    const getColorId = randomIntegerFromInterval(0, colors.length);
+    refs.body.style.background = colors[getColorId];
+  }
+let id = null;
+function startBtnClick() {
+   id=setInterval(timer, 1000);
+    refs.startBtn.disabled = true;
     
+}
+
+function stopBtnClick() {
+    refs.startBtn.disabled = false;
+    clearInterval(id);
+}
